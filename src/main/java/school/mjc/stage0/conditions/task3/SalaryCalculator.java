d4d4cedb28909fa7cb3f10987b00574e0b8ce74b
Roman Gulevich <R.I.Gulevich@gmail.com>
2022-07-01 00:26:23 +0300

    "Module4 - Task3"

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary > 0) {
+            if (salary <= 10_000) {
+                System.out.println(salary * 0.85);
+            }
+            if (salary > 10_000 & salary <= 20_000) {
+                System.out.println(salary * 0.82);
+            }
+            if (salary > 20_000) {
+                System.out.println(salary * 0.80);
+            }
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
